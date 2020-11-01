86d76966e65854e6794cea414e0a810da2f7793d
Abish Pius <60949830+abishpius@users.noreply.github.com>
2020-11-01 11:44:48 -0500

    Add files via upload

---
+++ ML_Projects/Blazor_WebApps/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.ML;
+using Sentiment_AnalysisML.Model;
+
+namespace Sentiment_Analysis.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly PredictionEnginePool<ModelInput, ModelOutput> _predictionEnginePool;
+
+        public IndexModel(PredictionEnginePool<ModelInput, ModelOutput> predictionEnginePool)
+        {
+            _predictionEnginePool = predictionEnginePool;
+        }
+       
+        public IActionResult OnGetAnalyzeSentiment([FromQuery] string text)
+        {
+            if (String.IsNullOrEmpty(text)) return Content("Neutral");
+            var input = new ModelInput { SentimentText = text };
+            var prediction = _predictionEnginePool.Predict(input);
+            var sentiment = Convert.ToBoolean(prediction.Prediction) ? "Toxic" : "Not Toxic";
+            return Content(sentiment);
+
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
